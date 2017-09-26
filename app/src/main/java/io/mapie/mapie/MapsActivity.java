82f23646ed74e26e0f05c4ca405b68ddb0381f30
Silas Stulz <sst@studer-raimann.ch>
2017-09-26 19:40:44 +0200

    New Profile page and button on map

--- app/src/main/java/io/mapie/mapie/MapsActivity.java
+++ app/src/main/java/io/mapie/mapie/MapsActivity.java
@@ -3,6 +3,7 @@ package io.mapie.mapie;
 
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
@@ -11,6 +12,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
@@ -36,6 +39,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -49,6 +53,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Marker mCurrLocationMarker;
     LocationRequest mLocationRequest;
     String value;
+    private Button btn_profile;
 
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -65,6 +70,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        btn_profile = (Button) findViewById(R.id.start_profile);
+
+        btn_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MapsActivity.this, ProfileActivity.class));
+            }
+        });
 
 
 
