c35c0857843ca2121041c0bb879ddec38de326bc
Silas Stulz <sst@studer-raimann.ch>
2017-09-27 15:59:48 +0200

    Cameraview now implemented

--- app/src/main/java/io/mapie/mapie/CameraActivity.java
+++ app/src/main/java/io/mapie/mapie/CameraActivity.java
@@ -5,13 +5,42 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+
+        try {
+            mCamera = Camera.open();
+        }
+        catch (Exception e){
+            Log.d("Error" , "Failed to open camera" + e.getMessage());
+        }
+
+        if(mCamera != null){
+            mCameraView = new CameraView(this, mCamera);
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);
+        }
+
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                System.exit(0);
+            }
+        });
 
     }
 
