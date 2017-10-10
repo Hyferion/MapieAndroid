753904a7a7fc53c4a4633c7d0db9a4e6875df781
Silas Stulz <sst@studer-raimann.ch>
2017-10-10 12:44:38 +0200

    init Request System

---
+++ app/src/main/java/io/mapie/mapie/MapieFirebaseInstanceIDService.java
@@ -0,0 +1,12 @@
+package io.mapie.mapie;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MapieFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+
+
+
+
+}
---
+++ app/src/main/java/io/mapie/mapie/MapieFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package io.mapie.mapie;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+
+
+
+public class MapieFirebaseMessagingService extends FirebaseMessagingService {
+
+
+
+}
